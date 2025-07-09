67902f4e26aa3a13db49b0752ba58589c71d2a64
Parshuram <parshuram@blitzcodesys.com>
2025-07-09 10:40:50 +0530

    max consecutive ones and missing number

---
+++ Day 9/missing_number.js
@@ -0,0 +1,53 @@
+// 268. Missing Number
+
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+// Example 1:
+
+// Input: nums = [3,0,1]
+
+// Output: 2
+
+// Explanation:
+
+// n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+// Example 2:
+
+// Input: nums = [0,1]
+
+// Output: 2
+
+// Explanation:
+
+// n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+// Example 3:
+
+// Input: nums = [9,6,4,2,3,5,7,0,1]
+
+// Output: 8
+
+// Explanation:
+
+// n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const missingNumber = function (nums) {
+  let n = nums.length;
+  let sum = (n * (n + 1)) / 2;
+  for (let num of nums) {
+    sum -= num;
+  }
+  return sum;
+};
+
+// Input:
+const nums = [3, 0, 1];
+console.log(missingNumber(nums));
+// Output: 2
+// Time Complexity: O(n)
+// Space Complexity: O(1)
