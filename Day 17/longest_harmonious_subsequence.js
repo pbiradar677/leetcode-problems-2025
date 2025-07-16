88705593ba8a3695dee1240ba0078a868d48479d
Parshuram <parshuram@blitzcodesys.com>
2025-07-17 00:12:31 +0530

    largest sum with k element and longest harmonious subsequence

---
+++ Day 17/longest_harmonious_subsequence.js
@@ -0,0 +1,67 @@
+// 594. Longest Harmonious Subsequence
+// We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
+
+// Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
+
+// Example 1:
+
+// Input: nums = [1,3,2,2,5,2,3,7]
+
+// Output: 5
+
+// Explanation:
+
+// The longest harmonious subsequence is [3,2,2,2,3].
+
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+
+// Output: 2
+
+// Explanation:
+
+// The longest harmonious subsequences are [1,2], [2,3], and [3,4], all of which have a length of 2.
+
+// Example 3:
+
+// Input: nums = [1,1,1,1]
+
+// Output: 0
+
+// Explanation:
+
+// No harmonic subsequence exists.
+
+// Constraints:
+
+// 1 <= nums.length <= 2 * 104
+// -109 <= nums[i] <= 109
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findLHS = function (nums) {
+  const frequencies = new Map();
+  // Count frequencies
+  for (const num of nums) {
+    frequencies.set(num, (frequencies.get(num) || 0) + 1);
+  }
+  // check num,num+1 pair
+  let longest = 0;
+  for (const [num, count] of frequencies.entries()) {
+    if (frequencies.has(num + 1)) {
+      const length = count + frequencies.get(num + 1);
+      longest = Math.max(longest, length);
+    }
+  }
+  return longest;
+};
+// Input:
+const nums = [1, 3, 2, 2, 5, 2, 3, 7];
+console.log(findLHS(nums));
+// Output: 5
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
