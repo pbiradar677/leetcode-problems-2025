fff90ad9581172a5b6b471b68c5d5bfa97a4e5be
Parshuram <parshuram@blitzcodesys.com>
2025-07-30 22:46:18 +0530

    add digits without loop/recursion

---
+++ Day 29/add_digits.js
@@ -0,0 +1,37 @@
+// 258. Add Digits
+
+// Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+// Example 1:
+
+// Input: num = 38
+// Output: 2
+// Explanation: The process is
+// 38 --> 3 + 8 --> 11
+// 11 --> 1 + 1 --> 2
+// Since 2 has only one digit, return it.
+// Example 2:
+
+// Input: num = 0
+// Output: 0
+
+// Constraints:
+
+// 0 <= num <= 231 - 1
+
+// Follow up: Could you do it without any loop/recursion in O(1) runtime?
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+const addDigits = function (num) {
+  if (num === 0) return 0;
+  return 1 + ((num - 1) % 9);
+};
+// Input:
+const num = 38;
+console.log(addDigits(num));
+// Output: 2
+//Time Complexity: O(1)
+//Space Complexity: O(1)
